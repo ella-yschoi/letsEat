f08d5f6b978efb59fa766fde922eea5fea0d7fb2
Ella Yeonsu Choi <happymondaysu@gmail.com>
2023-05-03 00:48:45 +0900

    feat: Add modal (but WIP)

--- src/App.js
+++ src/App.js
@@ -4,23 +4,24 @@ import logo from './logo.svg';
 import { useState } from 'react'; // 1. useState import 하기
 
 function App() {
-  let post = '〰️ 맛집 추천은 계속됩니다 〰️'; // 서버에서 가져온 데이터라고 가정
+  let post = '🤙 나만 아는 맛집 제보하기'; // 서버에서 가져온 데이터라고 가정
   
   // 2. React에서 자료를 잠깐 저장하는 state 만들기
   // 3. 만약 변수 사용하고 싶다면 let [a, b]
     // a는 state에 보관했던 자료가 나옴
     // b는 state 변경을 도와주는 함수이며, 이걸 써야 html 재렌더링이 잘 됨
-  let [title, setTitle] = useState(['한남동 맛집 추천', '성수동 맛집 추천', '평창동 맛집 추천']);
+  let [title, setTitle] = useState(['성수역 | 데이트', '회기역 | 모임', '압구정역 | 회식']);
   let [thumb, setThumb] = useState(0);
   
   return (
     // JSX: class 넣을 때 className으로 작성
     <div className="App"> 
-      <div className="black-nav">
+      <div className="yellow-nav">
         {/* JSX: style 넣을 때 style={{스타일명: '값'}} */}
-        <h4 style={ {color: 'white', fontSize: '18px'} }>🍚 Foodie Blog 🍚</h4>
+        <h4 style={ {color: 'white', fontSize: '18px'} }
+        > 🍔 같이 먹어 🍔 </h4>
       </div>
-      
+
       <button onClick={()=>{
         let sortTitle = [...title];
         sortTitle.sort();
@@ -34,9 +35,9 @@ function App() {
           // '[...]' -> 괄호를 벗겼다가 다시 씌워준 것이므로 독립적인 array 사본이므로 화살표도 달라짐
           // => state가 array/object라면 독립적 copy(shallow copy)본을 만들어서 state 변경 함수에 넣어 주어야 함!
           let editTitle = [...title]; 
-          editTitle[0] = '성수 맛집 추천'
+          editTitle[0] = '망원동 | 데이트'
           setTitle(editTitle);
-        }}>제목 수정</button>
+        }}> 이번주 데이트는 망원동 어때요? </button>
       
       {/* state는 갑자기 변경되면 state 쓰던 html은 자동으로 재렌더링 됨 
         따라서 state는 변동시 자동으로 html에 반영되게 만들고 싶을 때 사용하기 */}
@@ -45,25 +46,61 @@ function App() {
         <h4>{ title[0] } <span onClick={()=>{ 
           // setThumb 괄호 안에 있는건 기존 state를 갈아치워주는 역할
           // 다만, 기존 state === 변경 state -> 변경 X
-          setThumb(thumb+1) }}>👍🏻</span>
+          setThumb(thumb+1) }}> 👍 </span>
         { thumb }</h4>
-        <p>4월 1일 발행</p>
+        <p className="share">📮 공유하기</p>
       </div>
       
       <div className="list">
-        <h4>{ title[1] }<span>👍🏻</span>{ thumb }</h4>
-        <p>4월 2일 발행</p>
+        <h4>{ title[1] }<span> 👍 </span>{ thumb }</h4>
+        <p className="share">📮 공유하기</p>
       </div>
       
       <div className="list">
-        <h4>{ title[2] }<span>👍🏻</span>{ thumb }</h4>
-        <p>4월 3일 발행</p>
+        <h4>{ title[2] }<span> 👍 </span>{ thumb }</h4>
+        <p className="share">📮 공유하기</p>
       </div>
+
+      <Modal></Modal>
       
       {/* JSX: 변수 넣을 때 {} 사용 → Data Binding */}
       <h4>{ post }</h4> 
     </div>
   );
+}
+
+function Modal() {
+  return(
+    // 추후 컴포넌트로 넣을 것 ----------
+    <div className='modal'>
+      <h4>압구정역 회식하기 좋은 장소</h4>
+      <p>
+        📍 <b>위치</b><br/>
+        압구정역 근처
+        <p/>
+        😋 <b>음식</b><br/>
+        잠봉 크뤼<br/>
+        부드러운 달걀 스크램블에 짭쪼름한 프로슈토를,
+        <br/>비스킷같이 바삭한 빵에 올려 먹었다. 개인적으로 최애!
+        <p/>
+        와플 샌드위치<br/>
+        써니사이드 에그, 베이컨, 치즈로 만든 와플 샌드위치<br/>
+        아는 맛인줄 알았으나 은근한 새로움을 주어서 같이 간 3명의 최애
+        <p/>
+        🤑 <b>금액대</b><br/>
+        음식은 1만원 후반~2만원 초중반대
+        커피 및 음료는 7천원~1만원대
+        <p/>
+        😌 <b>분위기</b><br/>
+        여자들끼리 분위기 좋은데에서 이쁜 브런치 먹고 네 다섯 시간 수다 떨기엔 좋은 장소
+        <p/>
+        🤔 <b>총평</b> ⭐️⭐️⭐️⭐️ <br/>
+        법카로 사치부리고 싶다? 추천!<br/>
+        프랑스 여행하는 기분 내고 싶다? 추천!
+        <br/>
+        </p>
+    </div>
+  )
 }
 
 export default App;

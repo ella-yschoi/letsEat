2c89b558972b085169162778fab1a8086238ac79
Ella Yeonsu Choi <happymondaysu@gmail.com>
2023-05-13 02:01:58 +0900

    fix: Edit modal feature with title

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { useState } from 'react';
 
 function App() {
   let post = '🤙 나만 아는 맛집 제보하기'; 
-  let [title, setTitle] = useState(['성수역 | 데이트', '강남역 | 회식', '압구정역 | 모임']);
+  let [title, setTitle] = useState(['성수역', '강남역', '압구정역']);
   let [thumb, setThumb] = useState([0,0,0]);
   let [modal, setModal] = useState(false); 
   let [modalTitle, setModalTitle] = useState(0);
@@ -25,7 +25,7 @@ function App() {
     
       <button onClick={()=>{ 
           let editTitle = [...title]; 
-          editTitle[0] = '망원동 | 데이트'
+          editTitle[0] = '망원역'
           setTitle(editTitle);
         }}> 이번주 데이트는 망원동 어때요? </button>
       
@@ -33,7 +33,7 @@ function App() {
         title.map(function(a, i){ 
           return (
           <div className="list" key={i}> 
-          <h4 button onClick={() => { setModal(!modal) }}>{ title[i] }
+          <h4 button onClick={() => { setModal(!modal); setModalTitle(i) }}>{ title[i] }
           <span onClick={()=>{
             let copy = [...thumb];
             copy[i] = copy[i] + 1;
@@ -44,10 +44,6 @@ function App() {
           </div>)
         }) 
       }
-
-      <button onClick={()=>{setModalTitle(0)}}>성수역</button>
-      <button onClick={()=>{setModalTitle(1)}}>강남역</button>
-      <button onClick={()=>{setModalTitle(2)}}>압구정역</button>
 
       {modal === true ? 
       <Modal 
@@ -68,31 +64,24 @@ function Modal(props) {
       <h4>{ props.title[props.modalTitle] }</h4>
       <p>
         📍 <b>위치</b><br/>
-        압구정역 근처
+        { props.title[props.modalTitle] } 근처
         <p/>
         😋 <b>음식</b><br/>
-        잠봉 크뤼<br/>
-        부드러운 달걀 스크램블에 짭쪼름한 프로슈토를,
-        <br/>비스킷같이 바삭한 빵에 올려 먹었다. 개인적으로 최애!
-        <p/>
-        와플 샌드위치<br/>
-        써니사이드 에그, 베이컨, 치즈로 만든 와플 샌드위치<br/>
-        아는 맛인줄 알았으나 은근한 새로움을 주어서 같이 간 3명의 최애
+        음식 맛 후기
         <p/>
         🤑 <b>금액대</b><br/>
-        음식은 1만원 후반~2만원 초중반대
-        커피 및 음료는 7천원~1만원대
+        금액대 정보 입력
         <p/>
         😌 <b>분위기</b><br/>
-        여자들끼리 분위기 좋은데에서 이쁜 브런치 먹고 네 다섯 시간 수다 떨기엔 좋은 장소
+        분위기 설명
         <p/>
-        🤔 <b>총평</b> ⭐️⭐️⭐️⭐️ <br/>
-        법카로 사치부리고 싶다? 추천!<br/>
-        프랑스 여행하는 기분 내고 싶다? 추천!
-        <br/>
-        </p>
-        <button onClick={()=>{props.setTitle(['성수역 | 데이트', 
-        '강남역 | 회식', '압구정역에서 이거 먹자!'])}}>제목 수정해서 공유하기</button>
+        🤔 <b>총평</b><br/>
+        총평 후기
+        <p/>
+      </p>
+        <button onClick={()=>{props.setTitle(['성수역에서 이거 먹자', 
+        '강남역에서 이거 먹자', '압구정역에서 이거 먹자'])}}>제목 수정해서 공유하기</button><p/>
+
     </div>
   )
 }
